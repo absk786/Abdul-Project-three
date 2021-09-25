4edc76f714f072798c072630379dd5f896d950fa
absk786 <rehmank11@gmail.com>
2021-09-25 12:40:44 +0100

    usertypedef added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,22 +9,42 @@ const resolvers = {
         categories: async () => {
             return await Category.find()
             },
-        products: async () => {
-
+        
+            products: async (parent, {category, name}) => {
+            const params = {};
+            if (category) {
+                params.category =  category;
+            }
+            if (name) {
+                params.name = {
+                    $regex:name
+                };
+            }
+            return await Product.find(params).populate('category')
         },
-        product: async () => {
-
+        
+        product: async (parent, {_id}) => {
+            return await Product.findById(_id).populate('catergoy')
         },
-        user: async () => {
-
-        },
+        user: async (parent, args, context) => {
+            if (context.user) {
+              const user = await User.findById(context.user._id).populate({
+                path: 'orders.products',
+                populate: 'category'
+              });
+      
+              user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+              return user;
+            }
+      
+            throw new AuthenticationError('Not logged in');
+          },
         order: async () => {
 
         },
         checkout: async () => {
 
         }
-
     },
     Mutations:{
         addUser: async () => {
