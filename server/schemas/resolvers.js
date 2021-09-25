cde04997c071cb910887712f26b3c26ae2a8fcd3
absk786 <rehmank11@gmail.com>
2021-09-25 12:15:03 +0100

    resolvers started

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+const {AutenticationError } = require('apollo-server-express')
+const {User, Product, Category, Order} = require('../models')
+
+const {signToken} = require ('../utils/auth')
+
+const resolvers = { 
+
+    Query: {
+
+    },
+    Mutations:{
+
+    }
+
+}
