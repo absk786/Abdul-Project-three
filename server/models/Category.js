f0aa03819f3d32d1e4bc8c264736402dc6b2f864
absk786 <rehmank11@gmail.com>
2021-09-25 12:03:20 +0100

    schema done

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,15 +1,15 @@
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose');
 
-const {schema} =  mongoose;
+const { Schema } = mongoose;
 
-const categorySchema = new Schema ({
-    name: {
-        type: String,
-        required: true,
-        trim: tru
-      }
-})
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  }
+});
 
-const Category = mongoose.model('Category', categorySchema)
+const Category = mongoose.model('Category', categorySchema);
 
 module.exports = Category;
