aebc3af8888b1c53509e1ec3561dc7e704d0693a
absk786 <rehmank11@gmail.com>
2021-09-25 13:07:07 +0100

    typedefs done

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require ('mongoose')
-requre ('dotenv').config()
+require ('dotenv').config()
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const express = rquire('express') 
+const express = require('express') 
 const {ApolloServer} = require('apollo-server-express')
 const path = require('path')
 
