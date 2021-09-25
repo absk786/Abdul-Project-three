10fa00ce3f3a6b2796b8b7b49f12dba7dc6c917b
absk786 <rehmank11@gmail.com>
2021-09-25 11:43:47 +0100

    typedefs started

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,28 @@
+const {gql} = require('apollo-server-express')
+
+const typeDefs = gql`
+type Catogery {
+
+}
+type Product {
+
+}
+type Order {
+
+}
+type User {
+
+}
+type Checkout {
+
+}
+type Auth {
+
+}
+type Query {
+
+}
+type Mutation{
+    
+}
+`
