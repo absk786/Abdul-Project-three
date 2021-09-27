bbcc16a5a6904faf3d0a86332b48f050fa5f033c
absk786 <rehmank11@gmail.com>
2021-09-27 14:10:39 +0100

    changes added

--- client/src/App.js
+++ client/src/App.js
@@ -43,7 +43,7 @@ function App() {
       <Router>
         <div>
           <StoreProvider>
-            <Nav />
+            <Nav/>
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
@@ -59,5 +59,4 @@ function App() {
     </ApolloProvider>
   );
 }
-
 export default App;
