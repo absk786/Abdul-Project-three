8ed076e12ec0e20c67c988cb2d392e61fcf09f2b
absk786 <rehmank11@gmail.com>
2021-09-28 15:17:27 +0100

    nomatch removed

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
-import NoMatch from './pages/NoMatch';
+// import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
@@ -51,7 +51,7 @@ function App() {
               <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
-              <Route component={NoMatch} />
+              {/* <Route component={NoMatch} /> */}
             </Switch>
           </StoreProvider>
         </div>
