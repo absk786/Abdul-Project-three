5e66f1bb593f8fba35aec2239a94986be879497b
absk786 <rehmank11@gmail.com>
2021-09-29 08:57:14 +0100

    signup and nomach removed

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
-import NoMatch from './pages/NoMatch';
+// import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
@@ -47,11 +47,11 @@ function App() {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
+              {/* <Route exact path="/signup" component={Signup} /> */}
               <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
-              <Route component={NoMatch} />
+              {/* <Route component={NoMatch} /> */}
             </Switch>
           </StoreProvider>
         </div>
