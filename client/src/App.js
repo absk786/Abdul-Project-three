28a0186a007d4c332156376038d7428701d909b3
absk786 <rehmank11@gmail.com>
2021-09-29 09:00:49 +0100

    signup removed

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import Home from './pages/Home';
 import Detail from './pages/Detail';
 // import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
-import Signup from './pages/Signup';
+// import Signup from './pages/Signup';
 import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
