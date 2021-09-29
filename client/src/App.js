d1f8dadc1ec3b812a3afa2870f541c239ef95f46
absk786 <rehmank11@gmail.com>
2021-09-29 09:39:00 +0100

    no match readded

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,9 @@ import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
-// import NoMatch from './pages/NoMatch';
+import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
-// import Signup from './pages/Signup';
+import Signup from './pages/Signup';
 import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
@@ -47,11 +47,11 @@ function App() {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
-              {/* <Route exact path="/signup" component={Signup} /> */}
+              <Route exact path="/signup" component={Signup} />
               <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
-              {/* <Route component={NoMatch} /> */}
+              <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
         </div>
